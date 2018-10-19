4dcb32688d3ac784e4a8414365895e787e526831
Angela Sun <angela.sun@ryerson.ca>
2018-10-19 17:27:46 -0400

    firing projectiles done

---
+++ Unity/prototype1/Assets/ProjectileScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ProjectileScript : MonoBehaviour {
+    public Vector2 gravity = new Vector3(0f, -9.8f, 0f);
+    private float angle;
+    public float velocityX;
+    public float velocityY;
+
+    Rigidbody2D rb;
+
+        // Use this for initialization
+	void Start () {
+       rb = GetComponent<Rigidbody2D>();
+        angle = GameObject.Find("Aimer").transform.rotation.z;
+        float power = GameObject.Find("turret").GetComponent<PlayerMovement>().power;
+        velocityX = (float)( power * 0.2 * Math.Cos(angle));
+        velocityY = (float)( power * 0.2 * Math.Sin(angle));
+        Debug.Log(angle);
+        rb.velocity = new Vector2(velocityX, velocityY );
+    }
+
+    
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        rb.velocity += gravity * Time.fixedDeltaTime;
+ 	}
+}          
---
+++ Unity/prototype1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMovement : MonoBehaviour {
+    private float movement;
+    private Rigidbody2D rigidBody;
+    public Transform rotateLimitCheck;
+    public GameObject projectile;
+    public float power = 0f;
+
+
+
+
+    // Use this for initialization
+    Vector2 bulletPos;
+
+
+    void Start() {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        movement = Input.GetAxis("Vertical");
+        if (movement > 0f && rotateLimitCheck.rotation.z < 0.7) {
+            transform.parent.Rotate(0, 0, 1);
+        }
+        else if (movement < 0f && rotateLimitCheck.rotation.z > -0.4)
+        {
+            transform.parent.Rotate(0, 0, -1);
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            fire();
+            StartCoroutine(Wait());
+            
+        }
+        if (Input.GetButton("space"))
+        {
+            power++;
+           
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
+        power = 0f;
+    }
+
+
+    void fire()
+    {
+        bulletPos = transform.position;
+        GameObject proj = Instantiate(projectile, bulletPos, Quaternion.identity);
+       // Vector3 targetOffset = new Vector3(power, 0f, 0f);
+       // float a = rotateLimitCheck.rotation.z * Mathf.Deg2Rad; //convert angle to radian
+       // Debug.Log("The target offset is" + targetOffset);
+       // Vector3 dir = targetOffset - transform.position;
+       // float height = dir.y;
+       // dir.y = 0;
+       // float dist = dir.magnitude;
+       // dir.y = dist * Mathf.Tan(a);
+       // dist += height / Mathf.Tan(a);
+       // proj.GetComponent<Rigidbody2D>().velocity = dir.normalized * (float)Math.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+
+    }
+
+        
+}
